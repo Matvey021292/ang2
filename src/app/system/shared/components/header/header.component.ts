9d0ffc6332d9787590df18efec6c69c64971b6f4
raul021292@gmail.com <raul021292@gmail.com>
2018-04-20 18:48:52 +0300

    lst

---
+++ src/app/system/shared/components/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ps-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  date:Date = new Date();
+  constructor() { }
+
+  ngOnInit() {
+  }
+  onLogout(){
+
+  }
+
+}
